d711f1341907450256d9ee5a01f2fe6611468518
Jiawei Xu <114122108+jimmyxu123@users.noreply.github.com>
2024-06-11 12:34:53 +0800

    Update run_select.py

--- run_all.py
+++ run_all.py
@@ -3,29 +3,29 @@ import subprocess
 import shlex
 
 def run_script(script_name, arg_str):
-    """Run a python script with given arguments as a single string and capture its output."""
-    args = shlex.split(arg_str)  # Safely parse the string into a list of arguments
+    args = shlex.split(arg_str)  
     try:
         result = subprocess.run(
             ["python", script_name] + args,
-            text=True,
-            capture_output=True,
-            check=True  # This will raise an exception if the script exits with a non-zero status
+            check=True  
         )
-        print(f"Output of {script_name}:\n{result.stdout}")
     except subprocess.CalledProcessError as e:
         print(f"Error running {script_name}:\n{e.stderr}")
         raise
 
 def main(args):
-    print("Starting to run scripts...")
-    run_script("base_ood_eval.py", args.script1_args)
-    print("Script execution completed successfully.")
+    print("Starting SELECT benchmark...")
+    run_script("base-ood/base_ood_eval.py", args.base_ood)
+    run_script("vtab/TestAllDatasets.py", args.vtab)
+    run_script("ssl/eval_knn.py", args.ssl)
+    print("SELECT evaluation completed successfully.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run a Python script with flexible arguments.')
-    parser.add_argument('--base_ood_args', type=str, default='', help='Arguments for script1.py as a single quoted string')
-    
+    parser.add_argument('--base_ood', type=str, default='', help='base&ood args')
+    parser.add_argument('--vtab', type=str, default='', help='vtab')
+    parser.add_argument('--ssl', type=str, default='', help='ssl')
     args = parser.parse_args()
     main(args)
+
 
