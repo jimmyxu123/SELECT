6db9fe13af28ccde460d0b5624115f9eaf970ff0
Jiawei Xu <114122108+jimmyxu123@users.noreply.github.com>
2024-06-11 16:39:14 +0800

    Update run_select.py

--- run_select.py
+++ run_select.py
@@ -14,14 +14,14 @@ def run_script(script_name, arg_str):
         raise
 
 def main(args):
-    print("Starting SELECT benchmark...")
+    print("Starting SELECT evaluation...")
     run_script("base-ood/base_ood_eval.py", args.base_ood)
     run_script("vtab/TestAllDatasets.py", args.vtab)
     run_script("ssl/eval_knn.py", args.ssl)
     print("SELECT evaluation completed successfully.")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Run a Python script with flexible arguments.')
+    parser = argparse.ArgumentParser(description='Run select eval script with flexible arguments.')
     parser.add_argument('--base_ood', type=str, default='', help='base&ood args')
     parser.add_argument('--vtab', type=str, default='', help='vtab')
     parser.add_argument('--ssl', type=str, default='', help='ssl')
